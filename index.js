99ccb257456a288564f1e6638705547bdd2cd767
William <will_cable@hotmail.com>
2023-01-27 15:03:57 +0800

    Addition of major files and file organisation, basic classes with starting values and methods as-required in the project README. Basic functionality of class creation using constructors established

---
+++ index.js
@@ -0,0 +1,12 @@
+const Engineer = require("./lib/Engineer.class");
+const Intern = require("./lib/Intern.class");
+const Manager = require("./lib/Manager.class");
+
+const engineer = new Engineer();
+const intern = new Intern();
+const manager = new Manager();
+
+engineer;
+intern;
+manager;
+
